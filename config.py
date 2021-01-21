a54cbd2ce95fcfe98051db1bc86327b2e61e8430
victron777 <victor_hugogg@yahoo.com.mx>
2021-01-20 23:37:14 -0500

    second commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+DB_DETAILS = {
+    'dev': {
+        'SOURCE_DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': 'localhost',
+            'DB_NAME': 'sourceDB',
+            'DB_USER': os.environ.get('DB_USER'),
+            'DB_PASS': os.environ.get('DB_PASS')
+        },
+        'TARGET_DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': 'localhost',
+            'DB_NAME': 'targetDB',
+            'DB_USER': os.environ.get('DB_USER'),
+            'DB_PASS': os.environ.get('DB_PASS')
+        }
+    }
+
+}
+
+# print(DB_DETAILS['dev'])
+# print(os.environ.get('DB_USER'))
+
